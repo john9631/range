86f54593a9bb858b810a967a360ad250e4796637
John Lynch <2434319+john9631@users.noreply.github.com>
2022-08-14 17:32:45 +1000

    Update range_test.v
    Additions

--- range_test.v
+++ range_test.v
@@ -8,5 +8,12 @@ fn test_range() {
 	assert range(2, 10, -2) == []
 	assert range(10, 2, -2) == [10, 8, 6, 4]
 	assert range(10, 2, -3) == [10, 7, 4]
+	assert range(2.0) == [0.0, 1.0]
+	assert range(2.0, 3.0) == [2.0]
+	assert range(2.0, 11.0, 4.0) == [2.0, 6.0, 10.0]
+	assert range(12.0, 4.0, -4.0) == [12.0, 8.0]
 	assert range(11.0, 7.5, -0.5) == [11.0, 10.5, 10.0, 9.5, 9.0, 8.5, 8.0]
+	assert range(11.0, 7.5, 0.5) == []
+
+	// assert range(1, 2, 3, 4) == Please input 1 to 3 integers or floats: range(start, stop, step)
 }
