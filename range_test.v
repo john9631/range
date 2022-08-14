81efa946b0defaf91af008b833d10e4e87ae7001
John Lynch <2434319+john9631@users.noreply.github.com>
2022-08-14 15:19:20 +1000

    Update range_test.v
    Update for new range function

--- range_test.v
+++ range_test.v
@@ -1,17 +1,12 @@
-import range
+import range { range }
 
-// TODO: add more tests
 fn test_range() {
-	range.int({
-		stop: 10
-	}) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-	range.int({
-		start: 1
-		stop: 10
-	}) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-	range.int({
-		start: 1
-		stop: 10
-		step: 2
-	}) == [1, 3, 5, 7, 9]
+	assert range(10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	assert range(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	assert range(1, 10, 2) == [1, 3, 5, 7, 9]
+	assert range(20, 10) == []
+	assert range(2, 10, -2) == []
+	assert range(10, 2, -2) == [10, 8, 6, 4]
+	assert range(10, 2, -3) == [10, 7, 4]
+	assert range(11.0, 7.5, -0.5) == [11.0, 10.5, 10.0, 9.5, 9.0, 8.5, 8.0]
 }
